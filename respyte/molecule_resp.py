768e2bf51fb17297c25b3267e96846e15f7bb801
j-wags <jwagnerjpl@gmail.com>
2019-01-21 18:02:14 -0800

    Fixed a few import paths (they need to begin with respyte.)

--- respyte/molecule_resp.py
+++ respyte/molecule_resp.py
@@ -12,8 +12,8 @@ try:
     import openeye.oechem as oechem
 except ImportError:
     warn(' The Openeye module cannot be imported. ( Please provide equivGoups and listofpolar manually.)')
-from molecule import *
-from readinp_resp import Input
+from respyte.molecule import *
+from respyte.readinp_resp import Input
 from pathlib import Path
 
 bohr2Ang = 0.52918825 # change unit from bohr to angstrom
